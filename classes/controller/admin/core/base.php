0aad735d152cc1f05fc4dc9c0fb3b30a742661ad
digibart <bart@digibart.nl>
2011-03-14 12:04:23 +0100

    fix validation when saving object

--- classes/controller/admin/core/base.php
+++ classes/controller/admin/core/base.php
@@ -2,7 +2,7 @@
 
 /**
  * Controller_Admin_Core_Base class.
- * 
+ *
  * @extends Controller_Template
  * @package Killer-admin
  * @category Controller
@@ -17,34 +17,33 @@ class Controller_Admin_Core_Base extends Controller_Template {
 	public function __construct($request, $response)
 	{
 		parent::__construct($request, $response);
-		
-		$this->request = Request::current();		
+
+		$this->request = Request::current();
 		$this->session= Session::instance();
 	}
 
 	/**
 	 * checks if a user is allowed to open the requested page
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
 	public function before()
 	{
 		parent::before();
-		
+
 
 		//Check user auth and role
-		$action_name = $this->request->action();	
-		
+		$action_name = $this->request->action();
+
 		//denie access if uri does not exist in config admin.menu
 		if (!in_array($this->request->directory() . "/" . $this->request->controller(), array_merge((array) Kohana::config('admin.menu'), array("admin/dashboard")))) {
-			Message::instance()->error(ucfirst(__(Kohana::message('admin','page not found'))));
+			Message::instance()->error(ucfirst(__(Kohana::message('admin', 'page not found'))));
 			$this->request->redirect('admin/main/');
-		} else {		        
-	        if (($this->auth_required !== FALSE && Auth::instance()->logged_in($this->auth_required) === FALSE)
-	                || (is_array($this->secure_actions) && array_key_exists($action_name, $this->secure_actions) && 
-	                Auth::instance()->logged_in($this->secure_actions[$action_name]) === FALSE))
-				{
+		} else {
+			if (($this->auth_required !== FALSE && Auth::instance()->logged_in($this->auth_required) === FALSE)
+				|| (is_array($this->secure_actions) && array_key_exists($action_name, $this->secure_actions) &&
+					Auth::instance()->logged_in($this->secure_actions[$action_name]) === FALSE)) {
 				if (Auth::instance()->logged_in()) {
 					Message::instance()->error(ucfirst(__('access denied')));
 					$this->request->redirect('admin/main/');
@@ -55,35 +54,35 @@ class Controller_Admin_Core_Base extends Controller_Template {
 				$this->user = Auth::instance()->get_user();
 			}
 		}
-			
+
 
 		//set the template values
 		$this->template->title = "Admin";
 		$this->template->content = "";
 		$this->template->scripts = array();
-		
+
 		$this->controller_url = $this->request->directory() . "/" . $this->request->controller();
 
-				
+
 		//show the menu
 		if ( Auth::instance()->logged_in()) {
 			$menu = Kohana::config('admin.menu');
-			
+
 			if (count($menu) == 0) {
-				Message::instance()->info(Kohana::message('admin','menu not found'));
+				Message::instance()->info(Kohana::message('admin', 'menu not found'));
 				$menu = array();
-			} 
-			
+			}
+
 			$this->template->menu = View::factory('admin/menubar')->set("items", $menu);
 		} else {
-		 	$this->template->menu = "";
-		 }
+			$this->template->menu = "";
+		}
 	}
-	
-	
+
+
 	/**
 	 * creates the list-view of a object
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -92,7 +91,7 @@ class Controller_Admin_Core_Base extends Controller_Template {
 		$get = $_GET;
 		$page =  (isset($get['page'])) ? (int) $get['page']: 1;
 		$offset = (($page > 0 ) ? $page - 1 : 0) * 20;
-		
+
 		//converts $_GET['filter'] in an array
 		$filter = array();
 		if (isset($get['filter'])) {
@@ -111,21 +110,21 @@ class Controller_Admin_Core_Base extends Controller_Template {
 			$count->and_where($field, 'like', '%' . $value . '%');
 		}
 		$count = $count->count_all();
-		
-		
+
+
 		//display message if no objects found
 		if ($count == 0) {
 			$msg = strtolower(__('no :objects found',  array(':objects' => __($this->orm_name))) . ".");
-			
+
 			//display link 'clear filter'
-			if (isset($get['filter'])) {			
+			if (isset($get['filter'])) {
 				$query = Url::query(array('filter' => null));
 				$msg .= "&nbsp;" . html::anchor($this->controller_url . $query, __('clear filter'));
 			}
 			Message::instance()->info($msg);
 		}
-		
-		
+
+
 		//pagination
 		$pagination = new Pagination;
 		$page_config = $pagination->config_group('admin');
@@ -137,45 +136,45 @@ class Controller_Admin_Core_Base extends Controller_Template {
 			$query = Url::query(array('page' =>  $pagination->__get('total_pages')));
 			$this->request->redirect($this->controller_url . $query);
 		}
-		
-		
+
+
 		//collect the orm objects
 		$objects = (isset($this->base_object)) ? $this->base_object->reset(false) : ORM::factory($this->orm_name);
 		$objects
-			->offset($offset)
-			->limit(20);
+		->offset($offset)
+		->limit(20);
 
 		// and apply filters
 		foreach ($filter as $field => $value) {
 			$objects->and_where($field, 'like', '%' . $value . '%');
 		}
-		
+
 		// and apply sorting
-		if (Arr::get($_GET, 'sort') && array_key_exists($_GET['sort'], $objects->list_columns())) {		
-			$order = (Arr::get($_GET,'order', 'asc') == 'asc') ? 'asc' : 'desc';
-			$objects->order_by(Arr::get($_GET,'sort'),$order);
+		if (Arr::get($_GET, 'sort') && array_key_exists($_GET['sort'], $objects->list_columns())) {
+			$order = (Arr::get($_GET, 'order', 'asc') == 'asc') ? 'asc' : 'desc';
+			$objects->order_by(Arr::get($_GET, 'sort'), $order);
 		}
 		$objects = $objects->find_all();
 
 
 		$view = View::factory('admin/'.$this->orm_name.'_list')
-			->bind('objects', $objects)
-			->bind('auth_user', $this->user)		
-			->bind('filter', $filter)
-			->bind('count', $count)
-			->bind('controller_url', $this->controller_url)
-			->bind('pagination', $pagination);
+		->bind('objects', $objects)
+		->bind('auth_user', $this->user)
+		->bind('filter', $filter)
+		->bind('count', $count)
+		->bind('controller_url', $this->controller_url)
+		->bind('pagination', $pagination);
 
 
 		$this->template->content = $view;
 
-	}	
-	
+	}
 
-	
+
+
 	/**
 	 * create the objects-form new page
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -185,46 +184,46 @@ class Controller_Admin_Core_Base extends Controller_Template {
 
 		$object = ORM::factory($this->orm_name);
 		$object->values($this->session->get_once('post_data_' . $this->orm_name, array()));
-		
+
 		$this->template->content = View::factory('admin/'.$this->orm_name.'_form')
-			->set('referrer', $this->session->get('requested_url'))
-			->set('controller_url', $this->controller_url)
-			->set('auth_user', $this->user)
-			->bind($this->orm_name, $object);
+		->set('referrer', $this->session->get('requested_url'))
+		->set('controller_url', $this->controller_url)
+		->set('auth_user', $this->user)
+		->bind($this->orm_name, $object);
 
 	}
-	
+
 	/**
 	 * creates the objects-form edit page
-	 * 
+	 *
 	 * @access public
-	 * @param mixed $id. (default: null)
+	 * @param mixed   $id. (default: null)
 	 * @return void
 	 */
 	public function action_edit($id = null)
 	{
 		$this->template->title = ucfirst(__('edit :object', array(':object' => __($this->orm_name))));
-		
+
 		$object = (isset($this->base_object)) ? $this->base_object->reset(false) : ORM::factory($this->orm_name);
 		$object->where('id', '=', (int) $id)->find();
 
-		$this->template->content = View::factory('admin/'.$this->orm_name.'_form')		
-			->set('referrer', $this->session->get('requested_url'))
-			->set('controller_url', $this->controller_url)		
-			->set('auth_user', $this->user)
-			->bind($this->orm_name, $object);
+		$this->template->content = View::factory('admin/'.$this->orm_name.'_form')
+		->set('referrer', $this->session->get('requested_url'))
+		->set('controller_url', $this->controller_url)
+		->set('auth_user', $this->user)
+		->bind($this->orm_name, $object);
 
 		if (!$object->loaded()) {
 			Message::instance()->error(__(':object not found', array(':object' => __($this->orm_name))));
 			$this->request->redirect($this->session->get_once('requested_url'));
 		}
 	}
-	
+
 	/**
 	 * validate and save the input
-	 * 
+	 *
 	 * @access public
-	 * @param mixed $id. (default: null)
+	 * @param mixed   $id. (default: null)
 	 * @return void
 	 */
 	public function action_save($id = null)
@@ -237,35 +236,39 @@ class Controller_Admin_Core_Base extends Controller_Template {
 		$object = (isset($this->base_object)) ? $this->base_object : ORM::factory($this->orm_name);
 		$object->where('id', '=', (int) $id)->find();
 		$object->values($post);
-		
+
 		// add current user if object does not belongs to a user
 		if (array_key_exists('user_id', $object->list_columns()) && $object->user_id == null) {
 			$object->user_id = $this->user->id;
 		}
-		
-		if ($object->check()) {
+
+		try {
 			if ($object->save()) {
 				Message::instance()->succeed(__(':object saved'),  array(':object' => __($this->orm_name)));
 			} else {
 				Message::instance()->error(__(':object not saved'),  array(':object' => __($this->orm_name)));
 			}
 			$this->request->redirect($this->session->get_once('requested_url'));
-		} else {
+
+		} catch (ORM_Validation_Exception $e) {
 			$errorstring = "";
-			foreach ($object->validate()->errors('register') as $key => $msg) {
+			$errors = $e->errors('register');
+
+			foreach ($errors as $key => $msg) {
 				$errorstring .= $msg . "<br />";
 			}
 			$this->session->set('post_data_' . $this->orm_name, $post);
 			Message::instance()->error($errorstring);
-			$this->request->redirect(Request::$referrer);
+			$this->request->redirect($this->request->referrer());
 		}
+
 	}
-	
+
 	/**
 	 * delete a object
-	 * 
+	 *
 	 * @access public
-	 * @param mixed $id
+	 * @param mixed   $id
 	 * @return void
 	 */
 	public function action_delete($id)
@@ -285,7 +288,7 @@ class Controller_Admin_Core_Base extends Controller_Template {
 		$this->request->redirect($this->session->get_once('requested_url'));
 
 	}
-	
+
 
 
 }
