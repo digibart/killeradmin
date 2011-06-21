9c2ced3bc2bd72d57a5ebbcc1a4fdf1da58ad852
digibart <bart@digibart.nl>
2011-06-21 21:44:56 +0200

    config cache for killerjs

---
+++ config/cache.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return array
+(
+	'killerjs'    => array
+	(
+		'driver'             => 'file',
+		'cache_dir'          => APPPATH.'cache',
+		'default_expire'     => 3600,
+	)
+);
