c31ed88a045d6a52dc3a64c042468a6a79d3d44b
digibart <bart@digibart.nl>
2011-12-06 20:11:03 +0100

    show captcha if visitor tried to login 3 times in past 5 minutes

--- classes/controller/admin/core/main.php
+++ classes/controller/admin/core/main.php
@@ -17,22 +17,59 @@ class Controller_Admin_Core_Main extends Controller_Admin_Base {
 	 */
 	public function action_login()
 	{
+
+		$failures = (int) Cache::instance()->get(md5($_SERVER['REMOTE_ADDR']));
+		$captcha = Captcha::instance('admin');
+
 		$this->template->title = ucfirst(__('login'));
 		$this->template->content = View::factory('admin/form_login');
 
-		if ($_POST) {
+		// if visitor got the wrong username/password 3 times, then validate visitor with a captcha
+		if ($failures > 3)
+		{
+			$this->template->content->set('captcha', $captcha->render());
+		}
+
+		if ($_POST)
+		{
+			$post = new Validation($_POST);
+			$post->rule('username', 'not_empty')
+			->rule('captcha', 'Captcha::valid', array(Arr::get($_POST, 'captcha')));
+
+
+			if ($post->check())
+			{
 			//Instantiate a new user
 			$user = ORM::factory('user');
-			//Check Auth
 			$status = Auth::instance()->login(Arr::get($_POST,'username'), Arr::get($_POST, 'password'), Arr::get($_POST,'remember'));
 
-			//If the post data validates using the rules setup in the user model
-			if ($status) {
+
+				//If user is logged then redirect
+				if ($status)
+				{
 				Message::instance()->succeed(ucfirst(__('access granted')));
+					Cache::instance()->set(md5($_SERVER['REMOTE_ADDR']), 0 , 300);
 				Request::current()->redirect(Route::get('admin/base_url')->uri(array('controller' => 'main')));
-			} else {
+				} 
+				else
+				{
+					//save the login failure
+					Cache::instance()->set(md5($_SERVER['REMOTE_ADDR']), $failures + 1, 300);
+
 				Message::instance()->error(__('username or password incorrect'));
 			}
+
+		}
+			else
+			{
+				$errorstring = "";
+				foreach ($post->errors('validate') as $key => $error)
+				{
+					$errorstring .= $error . "<br>";
+				}
+				Message::instance()->error($errorstring);
+			}
+
 
 		}
 
