e5e7523b302cf47441db9c52d95892000430e877
digibart <bart@digibart.nl>
2011-12-06 20:11:58 +0100

    show captcha if visitor tried to reset password 3 times in past 5 minutes

--- classes/controller/admin/core/main.php
+++ classes/controller/admin/core/main.php
@@ -11,7 +11,7 @@ class Controller_Admin_Core_Main extends Controller_Admin_Base {
 
 	/**
 	 * user login
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -39,27 +39,27 @@ class Controller_Admin_Core_Main extends Controller_Admin_Base {
 
 			if ($post->check())
 			{
-			//Instantiate a new user
-			$user = ORM::factory('user');
-			$status = Auth::instance()->login(Arr::get($_POST,'username'), Arr::get($_POST, 'password'), Arr::get($_POST,'remember'));
+				//Instantiate a new user
+				$user = ORM::factory('user');
+				$status = Auth::instance()->login(Arr::get($_POST, 'username'), Arr::get($_POST, 'password'), Arr::get($_POST, 'remember'));
 
 
 				//If user is logged then redirect
 				if ($status)
 				{
-				Message::instance()->succeed(ucfirst(__('access granted')));
+					Message::instance()->succeed(ucfirst(__('access granted')));
 					Cache::instance()->set(md5($_SERVER['REMOTE_ADDR']), 0 , 300);
-				Request::current()->redirect(Route::get('admin/base_url')->uri(array('controller' => 'main')));
+					Request::current()->redirect(Route::get('admin/base_url')->uri(array('controller' => 'main')));
 				} 
 				else
 				{
 					//save the login failure
 					Cache::instance()->set(md5($_SERVER['REMOTE_ADDR']), $failures + 1, 300);
 
-				Message::instance()->error(__('username or password incorrect'));
-			}
+					Message::instance()->error(__('username or password incorrect'));
+				}
 
-		}
+			}
 			else
 			{
 				$errorstring = "";
@@ -77,42 +77,56 @@ class Controller_Admin_Core_Main extends Controller_Admin_Base {
 
 	/**
 	 * reset password and email it to user
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
 	public function action_forgot()
 	{
+		$failures = (int) Cache::instance()->get(md5($_SERVER['REMOTE_ADDR']));
+		$captcha = Captcha::instance('admin');
+
 		$this->template->title = ucfirst(__('forgot password'));
 		$this->template->content = View::factory('admin/form_password');
 
+		if ($failures > 3)
+		{
+			$this->template->content->set('captcha', $captcha->render());
+		}
 
-		if ($_POST) {
+		if ($_POST)
+		{
 			$post = new Validation($_POST);
 			$post->rule('username', 'not_empty')
-				->rule('username', 'min_length', array('username',5))
-				->rule('username', 'max_length', array('username',42))
-				->rule('email', 'email', array(Arr::get($_POST, 'email')));
+			->rule('username', 'min_length', array('username', 5))
+			->rule('username', 'max_length', array('username', 42))
+			->rule('email', 'email', array(Arr::get($_POST, 'email')))
+			->rule('captcha', 'Captcha::valid', array(Arr::get($_POST, 'captcha')));
 
-			if ($post->check()) {
+
+			if ($post->check())
+			{
 				$user = ORM::factory('user')
-					->where('username', '=', (string) $_POST['username'])
-					->where('email', '=', (string) $_POST['email'])
-					->find();
+				->where('username', '=', (string) $_POST['username'])
+				->where('email', '=', (string) $_POST['email'])
+				->find();
 
-				if ($user->loaded()) {
+				if ($user->loaded())
+				{
 					$user->resetPassword();
 					Request::current()->redirect(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login')));
-				} else {
+				} else
+				{
+					Cache::instance()->set(md5($_SERVER['REMOTE_ADDR']), $failures + 1, 300);
 					Message::instance()->error(__(':object not found', array(':object' => __('user'))));
 				}
 
-
-			} else {
+			} else
+			{
 				$errorstring = "";
-				foreach ($post->errors('validate') as $key => $error) {
+				foreach ($post->errors('validate') as $key => $error)
+				{
 					$errorstring .= $error . "<br>";
-					echo $error;
 				}
 				Message::instance()->error($errorstring);
 			}
@@ -121,7 +135,7 @@ class Controller_Admin_Core_Main extends Controller_Admin_Base {
 
 	/**
 	 * logout user
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
@@ -134,7 +148,7 @@ class Controller_Admin_Core_Main extends Controller_Admin_Base {
 
 	/**
 	 * if no page is loaded, show the dashboard
-	 * 
+	 *
 	 * @access public
 	 * @return void
 	 */
