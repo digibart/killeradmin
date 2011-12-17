2d0e67b7eb4e35bc10f62814805d49c945cfec97
digibart <bart@digibart.nl>
2011-12-17 12:30:23 +0100

    adjust view: reset password

--- views/admin/form_password.php
+++ views/admin/form_password.php
@@ -1,15 +1,22 @@
-<form method="post" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot'))); ?>" class="validate">
-<dl>
-	<dt><?php echo ucfirst(__('username'));?></dt>
-	<dd><input type="text" name="username" class="required"></dd>
-	<dt><?php echo  ucfirst(__('email'));?></dt>
-	<dd><input type="text" name="email" class="required email"></dd>
-<?php if (isset($captcha) && $captcha) : ?>
-	<dt><?php echo ucfirst(__('verification'));?></dt>
-	<dd><?php echo $captcha; ?>	<br>
-		<?php echo __('enter the code above');?>: <input type="text" name="captcha" class="required" size="6"></dd>
-<?php endif; ?>
+<div class="seven columns">
+
+	<form method="post" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot'))); ?>" class="validate">
+		<label for="username"><?php echo ucfirst(__('username'));?></label>
+		<input type="text" name="username" id="username" class="required">
+		
+		<label><?php echo  ucfirst(__('email'));?></label>
+		<input type="text" name="email" class="required email">
+		
+		<?php if (isset($captcha) && $captcha) : ?>
+			<label for="captcha" style="height: 100px"><?php echo ucfirst(__('security question'));?></label>
+			<?php echo $captcha; ?><br>
+			<?php echo __('enter the code above');?>:<br>
+			<input type="text" name="captcha" class="required" id="captcha" size="6">
+		<?php endif; ?>		
 	
-</dl>
-<button type="submit"><?php echo __('new password'); ?></button>
-</form>
+		<div class="button bar">
+			<?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login')), __('go back'), array('class' => 'button'));?>
+			<button type="submit" class="primary"><?php echo ucfirst(__('new password')); ?></button>
+		</div>
+	</form>
+</div>
