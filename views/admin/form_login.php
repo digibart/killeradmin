266c845c52b65990080c9e2b711e80fc020ed16d
digibart <bart@digibart.nl>
2011-12-17 12:30:03 +0100

    adjust view: login form

--- views/admin/form_login.php
+++ views/admin/form_login.php
@@ -1,17 +1,23 @@
-<form method="post" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login'))); ?>">
-<dl>
-	<dt><?php echo ucfirst(__('username'));?></dt>
-	<dd><input type="text" name="username" class="required"></dd>
-	<dt><?php echo  ucfirst(__('password'));?></dt>
-	<dd><input type="password" name="password" class="required"> (<?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot')), __('forgot password'));?>)</dd>
-<?php if (isset($captcha) && $captcha) : ?>
-	<dt><?php echo ucfirst(__('security question'));?></dt>
-	<dd><?php echo $captcha; ?>	<br>
-		<?php echo __('enter the code above');?>: <input type="text" name="captcha" class="required" size="6"></dd>
-<?php endif; ?>
-	<dt><?php echo  ucfirst(__('remember me'));?></dt>
-	<dd><input type="checkbox" name="remember" id="checkbox_1" class="checkbox" /></dd>
-
-</dl>
-<button type="submit"><?php echo __('login'); ?></button>
-</form>
+<div class="seven columns">
+	<form method="post" class="validate" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login'))); ?>">
+	
+		<label for="username"><?php echo ucfirst(__('username'));?></label>
+		<input type="text" name="username" id="username" class="required">
+		
+		<label for="password"><?php echo  ucfirst(__('password'));?><span><?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot')), __('forgot password'));?></span></label>
+		<input type="password" name="password" id="password" class="required">
+		
+		<?php if (isset($captcha) && $captcha) : ?>
+			<label for="captcha" style="height: 100px"><?php echo ucfirst(__('security question'));?></label>
+			<?php echo $captcha; ?><br>
+			<?php echo __('enter the code above');?>:<br>
+			<input type="text" name="captcha" class="required" id="captcha" size="6">
+		<?php endif; ?>		
+		
+		<div class="button bar">
+			<button type="submit" class="primary"><?php echo __('login'); ?></button>
+		</div>
+	
+	
+	</form>
+</div>
