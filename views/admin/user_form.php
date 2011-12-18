e5d0a53308627941eeb0c30eeb163001ba7fc5a6
digibart <bart@digibart.nl>
2011-12-18 10:31:38 +0100

    adjust view: user form

--- views/admin/user_form.php
+++ views/admin/user_form.php
@@ -1,31 +1,33 @@
-<form method="post" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'users', 'action' => 'save', 'id' =>  $user->id)));?>">
-    <div class="span-6 suffix-1">
-        <label><?php echo ucfirst(__('username'));?> <small>(<?php echo __(':min-:max characters', array(':min' => 5, ':max' => 8)); ?>)</small></label>
-        	<input type="text" name="username" value="<?php echo $user->username;?>"><br>
-        <label><?php echo ucfirst(__('email'));?></label>
-        	<input type="text" name="email" value="<?php echo $user->email; ?>"><br>
-        <label><?php echo ucfirst(__('roles'));?></label>
-        <?php 
-            foreach (ORM::factory('role')->find_all() as $role) {
-            	$checked = $user->has('roles',$role ) ? "checked=checked" : "";
-            	$disabled = ($user->id == $auth_user->id) ? "disabled=disabled" : "";
-            	echo "<input type=\"checkbox\" name=\"role[".$role->name."]\" $checked $disabled>" . $role->name . "<br>";
-            
-            }
-            
-        ?>
+<div class="six columns prefix-three last">
+	<form method="post" class="validate" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'users', 'action' => 'save', 'id' =>  $user->id)));?>">
+		<label for="username"><?php echo ucfirst(__('username'));?> <span><?php echo __(':min-:max characters', array(':min' => 5, ':max' => 8)); ?></span></label>
+		<input type="text" name="username" id="username" class="required" minlength="5" maxlength="8" value="<?php echo $user->username;?>"><br>
+	    
+		<label for="email"><?php echo ucfirst(__('email'));?></label>
+	    <input type="text" name="email" id="email" class="required email" value="<?php echo $user->email; ?>">
+	        
+		<label class="spacer"><?php echo ucfirst(__('roles'));?></label>
+	    <div><?php 
+	            foreach (ORM::factory('role')->find_all() as $role) {
+	            	$checked = $user->has('roles',$role ) ? "checked=checked" : "";
+	            	$disabled = ($user->id == $auth_user->id) ? "disabled=disabled" : "";
+	            	echo "<input type=\"checkbox\" name=\"role[".$role->name."]\" $checked $disabled>" . $role->name . "<br>";
+	            
+	            }
+	            
+	        ?>
+	    </div>
+	         	
+        <label><?php echo ucfirst(__('password'));?> <span><?php echo __(':min-:max characters', array(':min' => 8, ':max' => 100)); ?></span></label>
+        <input type="password" name="password" minlength="8" maxlength="100" value="">
         
-    </div>
-
-    <div class="span-5 last">
-        <label><?php echo ucfirst(__('password'));?> <small>(<?php echo __(':min-:max characters', array(':min' => 8, ':max' => 100)); ?>)</small></label> <input type="password" name="password" value="">
-        <label><?php echo ucfirst(__('confirm password'));?>:</label> <input type="password" name="password_confirm" value="">
-    </div>
-	
-   	<div class="span-24">
-   		<p><br>
-       		<button type="submit" class="button positive"><?php echo KillerAdmin::spriteImg('save');?><?php echo __('save'); ?></button>
-       		<?php echo html::anchor($referrer, __('go back')); ?>
-   		</p>
-   	</div>
-</form>
+        <label><?php echo ucfirst(__('confirm password'));?>:</label>
+        <input type="password" name="password_confirm" value="" minlength="8" maxlength="100">
+		
+	   	<div class="button bar">
+	       		<?php echo html::anchor($referrer, __('go back'), array('class' => 'button')); ?>
+	       		<button type="submit" class="button primary"><?php echo KillerAdmin::spriteImg('save');?><?php echo __('save'); ?></button>
+	   		
+	   	</div>
+	</form>
+</div>
