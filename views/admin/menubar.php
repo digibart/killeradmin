520775e7558b1c4d53098203dd3b2ae77ece52bb
digibart <bart@digibart.nl>
2011-12-18 10:32:21 +0100

    adjust view: menu bar

--- views/admin/menubar.php
+++ views/admin/menubar.php
@@ -1,12 +1,18 @@
+<nav>
 <?php
 
 foreach ($items as $title => $url) {
 
-	$class = (Route::get('admin/base_url')->uri(array('controller' => Request::current()->controller())) == $url) ? "positive" : "";
+	$class = (Route::get('admin/base_url')->uri(array('controller' => Request::current()->controller())) == $url) ? "primary" : "";
 	$class .= " button";
 	echo html::anchor($url, $title, array('class' => $class));
 
 }
 
 ?>
-<?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'logout')), ucfirst(__('logout')), array('class' => 'button')); ?>
+</nav>
+
+<div class="tools">
+	<?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'settings', 'action' => 'index')), KillerAdmin::SpriteImg('cog') , array('title' => __('settings'), 'class' => 'tooltip')); ?><br>
+	<?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'logout')), KillerAdmin::SpriteImg('open-door'), array('title' => __('logout'), 'class' => 'tooltip')); ?>
+</div>
