d06548281d7cc2a5d88bcc4ceab2f0b5d54f21a9
digibart <bart@digibart.nl>
2012-02-05 16:33:10 +0100

    imrpove css and html structure  forms
    by using dl, dt, and dd

--- views/admin/settings_index.php
+++ views/admin/settings_index.php
@@ -1,17 +1,21 @@
 <div class="twelve columns prefix-6">
 	<form method="post" autocomplete="off" class="validate" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller'=>'settings','action'=>'save')));?>">
-        <label><?php echo ucfirst(__('username'));?></label>
-		<span class="info"><?php echo $user->username;?></span>
-
-        <label><?php echo ucfirst(__('email'));?></label>
-      	<input type="text" name="email" class="required email" value="<?php echo $user->email; ?>">
+		<dl>
+			<dt class="eight columns"><label><?php echo ucfirst(__('username'));?></label></dt>
+			<dd class="twelve last columns"><?php echo $user->username;?></dd>
+			
+			<dt class="eight columns"><label><?php echo ucfirst(__('email'));?></label></dt>
+			<dd class="twelve last columns"><input type="text" name="email" class="required email" autocomplete="off" value="<?php echo $user->email; ?>"></dd>
        
-        <label><?php echo ucfirst(__('password'));?> <span><?php echo __(':min-:max characters', array(':min' => 5, ':max' => 8)); ?></span></label>
-        <input type="password" name="password" id="password" minlength="5" maxlength="100" value="">
-        <small><?php echo __(Kohana::message('admin','leave password empty'));?></small>
+	        <dt class="eight columns"><label><?php echo ucfirst(__('password'));?> <span><?php echo __(':min-:max characters', array(':min' => 5, ':max' => 8)); ?></span></label></dt>
+	        <dd class="twelve last columns"><input type="password" name="password" id="password" minlength="5" maxlength="100" autocomplete="off" value="">
+	        <small><?php echo __(Kohana::message('admin','leave password empty'));?></small></dd>
+	
+	        <dt class="eight columns"><label><?php echo ucfirst(__('confirm password'));?></label></dt>
+	        <dd class="twelve last columns"><input type="password" name="password_confirm" minlength="5" maxlength="100" value=""></dd>
+	        
+		</dl>
 
-        <label><?php echo ucfirst(__('confirm password'));?></label>
-        <input type="password" name="password_confirm" minlength="5" maxlength="100" value="">
 		
 		<div class="button bar">
        		<?php echo html::anchor($referrer, __('go back'), array('class' => 'nice button')); ?>
--- views/admin/user_form.php
+++ views/admin/user_form.php
@@ -1,32 +1,33 @@
 <div class="last twelve columns prefix-6">
 	<form method="post" class="validate" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'users', 'action' => 'save', 'id' =>  $user->id)));?>">
-		<label for="username"><?php echo ucfirst(__('username'));?> <span><?php echo __(':min-:max characters', array(':min' => 5, ':max' => 8)); ?></span></label>
-		<input type="text" name="username" id="username" class="required" minlength="5" maxlength="8" value="<?php echo $user->username;?>"><br>
-	    
-		<label for="email"><?php echo ucfirst(__('email'));?></label>
-	    <input type="text" name="email" id="email" class="required email" value="<?php echo $user->email; ?>">
+		<dl>
+			<dt class="twelve columns"><label for="username"><?php echo ucfirst(__('username'));?> <span><?php echo __(':min-:max characters', array(':min' => 5, ':max' => 8)); ?></span></label></dt>
+			<dd class="twelve last columns"><input type="text" name="username" id="username" class="required" minlength="5" maxlength="8" autocomplete="off" value="<?php echo $user->username;?>"></dd>
+		    
+			<dt class="twelve columns"><label for="email"><?php echo ucfirst(__('email'));?></label></dt>
+		    <dd class="twelve last columns"><input type="text" name="email" id="email" class="required email" autocomplete="off" value="<?php echo $user->email; ?>"></dd>
+		        
+			<dt class="twelve columns"><label class="spacer"><?php echo ucfirst(__('roles'));?></label></dt>
+		    <dd class="twelve last columns"><div><?php 
+		            foreach (ORM::factory('role')->find_all() as $role) {
+		            	$checked = $user->has('roles',$role ) ? "checked=checked" : "";
+		            	$disabled = ($user->id == $auth_user->id) ? "disabled=disabled" : "";
+		            	echo "<input type=\"checkbox\" name=\"role[".$role->name."]\" $checked $disabled>" . $role->name . "<br>";
+		            
+		            }
+		            
+		        ?>
+		    </div></dd>
+		         	
+	        <dt class="twelve columns"><label><?php echo ucfirst(__('password'));?> <span><?php echo __(':min-:max characters', array(':min' => 8, ':max' => 100)); ?></span></label></dt>
+	        <dd class="twelve last columns"><input type="password" name="password" minlength="8" maxlength="100" autocomplete="off" value=""></dd>
 	        
-		<label class="spacer"><?php echo ucfirst(__('roles'));?></label>
-	    <div><?php 
-	            foreach (ORM::factory('role')->find_all() as $role) {
-	            	$checked = $user->has('roles',$role ) ? "checked=checked" : "";
-	            	$disabled = ($user->id == $auth_user->id) ? "disabled=disabled" : "";
-	            	echo "<input type=\"checkbox\" name=\"role[".$role->name."]\" $checked $disabled>" . $role->name . "<br>";
-	            
-	            }
-	            
-	        ?>
-	    </div>
-	         	
-        <label><?php echo ucfirst(__('password'));?> <span><?php echo __(':min-:max characters', array(':min' => 8, ':max' => 100)); ?></span></label>
-        <input type="password" name="password" minlength="8" maxlength="100" value="">
-        
-        <label><?php echo ucfirst(__('confirm password'));?>:</label>
-        <input type="password" name="password_confirm" value="" minlength="8" maxlength="100">
+	        <dt class="twelve columns"><label><?php echo ucfirst(__('confirm password'));?>:</label></dt>
+	        <dd class="twelve last columns"><input type="password" name="password_confirm" value="" minlength="8" maxlength="100" autocomplete="off"></dd>
 		
-	   	<div class="button bar">
-	       		<?php echo html::anchor($referrer, __('go back'), array('class' => 'nice button')); ?>
-	       		<button type="submit" class="nice primary button"><?php echo KillerAdmin::spriteImg('save');?><?php echo __('save'); ?></button>	   		
-	   	</div>
+			
+			<dt class="twelve columns"><?php echo html::anchor($referrer, __('go back'), array('class' => 'nice button')); ?></dt>
+	       	<dd class="twelve columns last"><button type="submit" class="nice primary button"><?php echo KillerAdmin::spriteImg('save');?><?php echo __('save'); ?></button></dd>
+   		</dl>
 	</form>
 </div>
