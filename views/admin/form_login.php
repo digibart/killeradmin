81a9f6debb75e3dd15ed3d2bfd6476cf689108a4
digibart <bart@digibart.nl>
2012-02-05 23:09:51 +0100

    fix layout view login form

--- views/admin/form_login.php
+++ views/admin/form_login.php
@@ -1,25 +1,25 @@
 <div class="last twelve columns prefix-6">
 	<form method="post" class="validate" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login'))); ?>">
-	
-		<label for="username"><?php echo ucfirst(__('username'));?></label>
-		<input type="text" name="username" id="username" class="required">
+		<dd>
+			<dt class="eight columns"><label for="username"><?php echo ucfirst(__('username'));?></label></dt>
+			<dd class="sixteen columns last"><input type="text" name="username" id="username" class="required"></dd>
 		
-		<label for="password"><?php echo  ucfirst(__('password'));?></label>
-		<input type="password" name="password" id="password" class="required">
-		<small><?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot')), __('forgot password'));?></small>
-		
-		
-		<?php if (isset($captcha) && $captcha) : ?>
-		<label for="captcha"><?php echo ucfirst(__('security question'));?></label>
-			<span class="info">
-				<?php echo $captcha; ?><br>
-				<?php echo __('enter the code above');?>:
-			<input type="text" name="captcha" class="required" id="captcha" size="6">
-			</span>
-		<?php endif; ?>		
-		
-		<div class="button bar">
-			<button type="submit" class="nice primary"><?php echo __('login'); ?></button>
-		</div>
+			<dt class="eight columns"><label for="password"><?php echo  ucfirst(__('password'));?></label></dt>
+			<dd class="sixteen columns">
+				<input type="password" name="password" id="password" class="required">
+				<small><?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot')), __('forgot password'));?></small>
+			</dd>
+				
+			<?php if (isset($captcha) && $captcha) : ?>
+			<dt class="eight columns"><label for="captcha"><?php echo ucfirst(__('security question'));?></label></dt>
+			<dd class="sixteen columns last">								
+						<?php echo $captcha; ?><br>
+						<?php echo __('enter the code above');?>:
+				<input type="text" name="captcha" class="required" id="captcha" size="6">				
+			</dd>
+			<?php endif; ?>
+			
+			<dd class="sixteen last columns prefix-8"><button type="submit" class="nice primary"><?php echo __('login'); ?></button></dd>
+		</dd>
 	</form>
 </div>
