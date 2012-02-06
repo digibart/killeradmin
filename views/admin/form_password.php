351fc1710d57ccc65084e9ea0a67a3ad4d95c799
digibart <bart@digibart.nl>
2012-02-06 11:44:00 +0100

    fix layout view reset password form

--- views/admin/form_password.php
+++ views/admin/form_password.php
@@ -1,23 +1,23 @@
 <div class="last twelve columns prefix-6">
 	<form method="post" action="<?php echo url::site(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'forgot'))); ?>" class="validate">
-		<label for="username"><?php echo ucfirst(__('username'));?></label>
-		<input type="text" name="username" id="username" class="required">
+
+		<dt class="eight columns"><label  for="username"><?php echo ucfirst(__('username'));?></label>
+		<dd class="sixteen columns last"><input type="text" name="username" id="username" class="required">
 		
-		<label><?php echo  ucfirst(__('email'));?></label>
-		<input type="text" name="email" class="required email">
+		<dt class="eight columns"><label ><?php echo  ucfirst(__('email'));?></label>
+		<dd class="sixteen columns last"><input type="text" name="email" class="required email">
 		
 		<?php if (isset($captcha) && $captcha) : ?>
-			<label for="captcha"><?php echo ucfirst(__('security question'));?></label>
-			<span class="info">
-			<?php echo $captcha; ?><br>
-			<?php echo __('enter the code above');?>:<br>
-			<input type="text" name="captcha" class="required" id="captcha" size="6">
-			</span>
+		<dt class="eight columns"><label  for="captcha"><?php echo ucfirst(__('security question'));?></label>
+		<dd class="sixteen columns last">
+		<?php echo $captcha; ?><br>
+		<?php echo __('enter the code above');?>:<br>
+		<input type="text" name="captcha" class="required" id="captcha" size="6">
+		</dd>
 		<?php endif; ?>		
 	
-		<div class="button bar">
-			<?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login')), __('go back'), array('class' => 'button'));?>
-			<button type="submit" class="nice primary"><?php echo ucfirst(__('new password')); ?></button>
+		<dt class="eight columns"><?php echo html::anchor(Route::get('admin/base_url')->uri(array('controller' => 'main', 'action' => 'login')), __('go back'), array('class' => 'button'));?></dt>
+		<dd class="sixteen columns last"><button type="submit" class="nice primary"><?php echo ucfirst(__('new password')); ?></button></dd>
 		</div>
 	</form>
 </div>
